25f5def2093468ce84abbb7d84f4ec62c59aea57
André Nunes <andrestanoga12@gmail.com>
2025-06-15 17:44:13 -0300

    deploy

---
+++ Back-End/src/main/java/unisanta/br/studia/WebConfig.java
@@ -0,0 +1,19 @@
+package unisanta.br.studia;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://stud-ia-h53t-e7373ucwu-andrenunes7s-projects.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
